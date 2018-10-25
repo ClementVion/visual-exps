8173e5cf515cc1b825353200228565c4d75fdbb7
Clément Vion <clement.vion49@gmail.com>
2018-10-25 23:01:57 +0200

    Circles wallah c sales

--- src/js/main.js
+++ src/js/main.js
@@ -1,7 +1,7 @@
 import * as THREE from 'three';
 
 let scene = new THREE.Scene();
-scene.background = new THREE.Color(0xdfe1e0);
+scene.background = new THREE.Color(0xdce0e1);
 let camera = new THREE.PerspectiveCamera(75, window.innerWidth / window.innerHeight, 0.1, 1000);
 camera.position.z = 5;
 let renderer = new THREE.WebGLRenderer();
@@ -27,12 +27,12 @@ init();
 function init() {
 
 	for (let i = 0; i < max; i += 1) {
-		geometries[i] = new THREE.PlaneGeometry(0.025, 2, 10, 10);
-		materials[i] = new THREE.MeshBasicMaterial({color: 0x4e4e4e, side: THREE.DoubleSide});
+		geometries[i] = new THREE.CircleGeometry(1, 32);
+		materials[i] = new THREE.MeshBasicMaterial({color: 0x000000, side: THREE.DoubleSide});
 		planes[i] = new THREE.Mesh(geometries[i], materials[i]);
-		planes[i].position.x = -1.25 + (Math.PI / max) * i
-		planes[i].position.y = (Math.PI / max)
-		planes[i].rotation.z = (max + 360) * i
+		// planes[i].position.x = -1.25 + (Math.PI / max) * i
+		// planes[i].position.y = -1.25 + (Math.PI / max) * i
+		planes[i].rotation.z = (max / 360) * i
 		scene.add(planes[i]);
 	}
 
@@ -45,8 +45,9 @@ function render() {
 	analyser.getByteFrequencyData(frequencyData);
 
 	for (let i = 0; i < max; i += 1) {
-		// planes[i].rotation.z += frequencyData[4] * 0.00001
-		planes[i].scale.set(0.25 + frequencyData[i] / 1000, 1 + frequencyData[i] / 200, 1)
+		planes[i].rotation.z += frequencyData[4] * 0.00001
+		planes[i].scale.set(1, 1 + frequencyData[i] / 200, 1)
+		planes[i].material.color.setRGB((frequencyData[i + 100]/255),(frequencyData[i + 100]/255),(frequencyData[i + 100]/255));
 	}
 }
 
