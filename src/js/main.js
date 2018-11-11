8fdf97f6f9f9dc9d4a9d091fecd84620abce09e7
Clément Vion <clement.vion49@gmail.com>
2018-11-11 22:10:56 +0100

    jonhopkins

--- src/js/main.js
+++ src/js/main.js
@@ -3,7 +3,7 @@ import * as THREE from 'three';
 let scene = new THREE.Scene();
 scene.background = new THREE.Color(0x0b0b0d);
 let camera = new THREE.PerspectiveCamera(75, window.innerWidth / window.innerHeight, 0.1, 1000);
-camera.position.x = 12
+camera.position.x = 0
 camera.position.y = 0;
 camera.position.z = 5;
 // camera.lookAt(0, 0, 0)
@@ -22,27 +22,26 @@ let frequencyData = new Uint8Array(analyser.frequencyBinCount);
 
 let geometries = [];
 let materials = []
-let planes = [];
 let objs = [];
-let max = 40
+let max = 60
 
 init();
 
 function init() {
 
-	for (let i = -max; i < max; i += 1) {
+	for (let i = 0; i < max; i += 1) {
 
-		materials[i] = new THREE.MeshBasicMaterial({
-			color: 0xb4b4b6
-		})
+		materials[i] = new THREE.LineBasicMaterial( { color: 0xffffff } );
 
-		geometries[i] = new THREE.PlaneGeometry(0.25, 15, 40, 40);
-		planes[i] = new THREE.Mesh(geometries[i], materials[i]);
+		geometries[i] = new THREE.Geometry();
 
-		planes[i].rotation.z = -Math.PI / 4
-		planes[i].position.x = i * 0.75
+		for (let v = -5; v < 5; v += 0.1) {
+			geometries[i].vertices.push(new THREE.Vector3(v, 0, 0))
+		}
 
-		scene.add(planes[i])
+		objs[i] = new THREE.Line(geometries[i], materials[i]);
+
+		scene.add(objs[i])
 	}
 
 	animate();
@@ -55,7 +54,7 @@ function displaceVertices(obj, dX, dY, dZ, size, magnitude, speed, ts) {
     let vertice = obj.geometry.vertices[i]
     let distance = new THREE.Vector3(vertice.x, vertice.y, vertice.z).sub(new THREE.Vector3(dX, dY, dZ))
 
-    vertice.z = Math.sin(distance.length() / size + (ts/speed)) * magnitude
+    vertice.y = Math.sin(distance.length() / size + (ts/speed)) * magnitude
   }
 
   obj.geometry.verticesNeedUpdate = true
@@ -67,10 +66,10 @@ function render(ts) {
 
 	analyser.getByteFrequencyData(frequencyData);
 
-	for (let i = -max; i < max; i += 1) {
+	for (let i = 0; i < max; i += 1) {
 
 		displaceVertices(
-			planes[i],
+			objs[i],
 			0, //dX
 		  0, //dY
 		  1, //dZ
@@ -80,7 +79,8 @@ function render(ts) {
 			ts
 		)
 
-		// planes[i].rotation.z += frequencyData[i] * 0.00001
+		objs[i].rotation.z += frequencyData[i] * 0.0001
+		objs[i].scale.x = frequencyData[i] * 0.001
 	}
 
 }
